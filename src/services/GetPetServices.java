ad56e7054d5579a1c88a0b580c5725d242d23e91
zoharmo <morz3212@gmail.com>
2017-05-20 09:28:53 +0300

    GetPetServices - dogList test for future use

--- src/services/GetPetServices.java
+++ src/services/GetPetServices.java
@@ -17,8 +17,11 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.google.gson.Gson;
 
+import KNN.Knn;
+import crud.Dogs;
 import crud.Users;
 import entities.Dog;
+import entities.DogAdopter;
 import entities.User;
 import enums.Animals;
 import enums.Area;
@@ -35,66 +38,18 @@ import enums.Hobbies;
 import enums.HouseType;
 import enums.Location;
 import enums.Relation;
+import enums.Size;
 
 @Path("/GetPetServices")
 public class GetPetServices {
 	private Gson gson = new Gson();
 	
-	@POST
-    @Path("/userRegistration") 
-	@Consumes(MediaType.APPLICATION_JSON)
-	@Produces(MediaType.TEXT_HTML)
-	public String userRegistration(InputStream incomingData) {
-		BufferedReader in = new BufferedReader(new InputStreamReader(incomingData));
-		User usr = gson.fromJson(in, User.class);
-		
-		if (Users.getUserByUserName(usr.getUserName()) != null){
-			return "ERROR: already exist user name " + usr.getUserName();
-		}else{
-			int returnCode = Users.save(usr);
-			if(returnCode == -1){
-				return "ERROR: problem with save user to db";}
-		}
-		return "OK";
-	}
-	
-	@GET
-    @Path("/checkUserNameValid") 
-	@Consumes(MediaType.TEXT_HTML)
-	public Boolean checkUserName(@QueryParam("userName")String userName) {
-		  return(!Users.doesUserNameExist(userName));
-	}
-	
-	@POST
-    @Path("/getUserAfterRegistration") 
-	@Consumes(MediaType.APPLICATION_JSON)
-	@Produces("application/json;charset=utf-8")
-	public String getUserAfterRegistration(InputStream incomingData) {
-		BufferedReader in = new BufferedReader(new InputStreamReader(incomingData));
-		Gson g = new Gson();
-		User usr = g.fromJson(in, User.class);
-		String s = g.toJson(usr);
-		return s;
-	}
-	
 
 	@POST 
-    @Path("/getDogAfterUpload;charset=utf-8") 
+    @Path("/getDogs") 
 	@Consumes(MediaType.APPLICATION_JSON)
-	@Produces("application/json")
-	public String getDogAfterUpload(InputStream incomingData) {
-		BufferedReader in = new BufferedReader(new InputStreamReader(incomingData));
-		Gson g = new Gson();
-		Dog dog = g.fromJson(in, Dog.class);
-		String s = g.toJson(dog);
-		return s;
-	}
-	
-	@POST 
-    @Path("/getDogsByAdoptionDetails") 
-	@Consumes(MediaType.APPLICATION_JSON)
-	@Produces("application/json;charset=utf-8")
-	public String getDogsByAdoptionDetails(InputStream incomingData) {
+	@Produces("applicationB/json;charset=utf-8")
+	public String getDogsTest(InputStream incomingData) {
 		BufferedReader in = new BufferedReader(new InputStreamReader(incomingData));
 		Gson g = new Gson();
 		User usr = g.fromJson(in, User.class);
@@ -105,28 +60,183 @@ public class GetPetServices {
 		
 		ArrayList<Dog> dogs = new ArrayList<>();
 		Dog d1 = new Dog();
-		d1.setName("rexi");
-		d1.setAge(4);
-		d1.setId("123");
+		d1.setId("1");
+		d1.setName("פו");
+		d1.setColor("Brown");
+		d1.setAge((short)1.5);
+		d1.setArea(Area.CENTER);
+		d1.setBreed(DogBreeds.Pomeranian);
+		User user1 = new User("ליאו","מסי");		
+		//d1.setOwner(user1);
+	//	d1.setPicture("../../assets/images/1.jpg");
+		d1.setDescription("פו ,כלבת פומרניאן מלאכית ומיוחדת. מלאני חברותית ומסתדרת מצוין עם כלבים ובני אדם, בת כשנה וחצי ובגודל קטן. מלאני מעוקרת ומחוסנת מלא ותמסר בסל אימוץ. נמצאת באומנה באיזור המרכז.");
+		d1.setGender(Gender.FEMALE);
+		d1.setSize(Size.SMALL);
+		dogs.add(d1);
 		
 		Dog d2 = new Dog();
-		d2.setName("lasi");
-		d2.setAge(3);
-		d2.setId("1234");
-		
+		d2.setId("2");
+		d2.setName("טופי");
+		d2.setColor("White");
+		d2.setAge((short)0.4);
+		d2.setArea(Area.CENTER);
+		d2.setBreed(DogBreeds.BullTerrier);
+		User user2 = new User("כריסטיאנו","רונלדו");		
+	//	d2.setOwner(user2);
+		//d2.setPicture("../../assets/images/2.jpg");
+		d2.setDescription("לאימוץ!!! טופי בת ה-4 חודשים. טופי תגדל לגודל בינוני ותימסר תמורת סל אימוץ. טופי מתוקה ומסתדרת עם כולם ובייחוד עם ילדים. טופי יודעת לעשות צרכים על עיתון. היא טובה ומקסימה ותתאים לכולם.");
+		d2.setGender(Gender.FEMALE);
+		d2.setSize(Size.MEDIUM);
 		dogs.add(d2);
-		dogs.add(d1);
+		
+		Dog d5 = new Dog();
+		d5.setId("5");
+		d5.setName("ג'וי");
+		d5.setColor("Brown");
+		d5.setAge((short)4);
+		d5.setArea(Area.CENTER);
+		d5.setBreed(DogBreeds.AnatolianShepherd);
+		User user5 = new User("קרוס","טוני");		
+	//	d5.setOwner(user5);
+	//	d5.setPicture("../../assets/images/5.jpg");
+		d5.setDescription("לא תושיטו גם לג'וי יד? ג'וי ננטשה בהסגר אחרי שהיתה בבית 4 שנים... עברה אילוף דרך העמותה והיא אידאלית לבית עם זוג צעיר. בת 4 שנים, מעוקרת ומחוסנת, ועם לב גדול ורחב.. אוהבת לשחק ואוהבת לאהוב!!! מי שיכול בבקשה לעזור..");
+		d5.setGender(Gender.FEMALE);
+		d5.setSize(Size.MEDIUM);
+		dogs.add(d5);
+		
+		Dog d7 = new Dog();
+		d7.setId("7");
+		d7.setName("בונו");
+		d7.setColor("White");
+		d7.setAge((short)0.5);
+		d7.setArea(Area.CENTER);
+		d7.setBreed(DogBreeds.Basenji);
+		User user7 = new User("בייל","גארת");		
+	//	d7.setOwner(user7);
+		//d7.setPicture("../../assets/images/7.jpg");
+		d7.setDescription("בונו גור בן חמישה חודשים משוטט באחד המושבים. המשפחה ששמה לו קולר מאכילה אותו פעם ביום אבל הוא לא יכול להכנס אליהם כי יש להם כלב תוקפן אז הוא ישן מתחת למבנה. מתאים למשפחה עם ילדים!! הוא עדין ומעט חששן אבל טוב. כל כך טוב ועדין שהוא לא ישרוד את החיים המחוספסים בחוץ.");
+		d7.setGender(Gender.MALE);
+		d7.setSize(Size.SMALL);
+		dogs.add(d7);
+		
+		Dog d9 = new Dog();
+		d9.setId("9");
+		d9.setName("כורכום");
+		d9.setColor("Brown");
+		d9.setAge((short)4);
+		d9.setArea(Area.CENTER);
+		d9.setBreed(DogBreeds.MiniaturePinscher);
+		User user9 = new User("קרבחאל","דני");		
+	//	d9.setOwner(user9);
+	//	d9.setPicture("../../assets/images/9.jpg");
+		d9.setDescription("כורכום, עוד שנה ועוד שנה ועוד שנה ואין לך בית. פינצ'ר קטן ומתוק שלא עשה שום דבר רע בחייו ונאלץ לחכות שוב ושוב לבית המתאים. כורכום הקטן בן 4 וחווה נטישה ששברה לו את הלב. עכשיו אחרי כל כך הרבה זמן, גם תורו הגיע. הוא מחונך, מחוסן ומסורס. מסתדר עם כלבים וחתולים. כל מה שהוא רוצה זה אהבה! מה יותר מזה? **נמצא כרגע ברמת גן");
+		d9.setGender(Gender.MALE);
+		d9.setSize(Size.SMALL);
+		dogs.add(d9);
+		
+		//----------------------
+		
+		
+		Dog d3 = new Dog();
+		d3.setId("3");
+		d3.setName("סולי");
+		d3.setColor("White");
+		d3.setAge((short)3.5);
+		d3.setArea(Area.NORTH);
+		d3.setBreed(DogBreeds.LabradorRetriever);
+		User user3 = new User("מסיקה","מירי");		
+	//	d3.setOwner(user3);
+	//	d3.setPicture("../../assets/images/3.jpg");
+		d3.setDescription("סולי שמו, וכשמו כן הוא - כלב נשמה. כשהוא אוהב זה מכל הלב, מסירות ללא גבול ונאמנות עד אין קץ. סולי עבר קורס אילוף ומתאים להובלת אנשים בעלי מוגבלויות. אז איך זה שמהיותו גור הוא רק מתחנן שמישהו יבחין בו? אני לא אוותר ואמשיך להפיץ אותו עד שיגיע האחד שירים את הכפפה ויציל את סולי ויזכה בחבר מושלם ואוהב. סולי מסורס, משובב מחוסן בכל החיסונים, מחונך לצרכים. מסתדר עם כל הכלבים. אינו כלב שמירה, ולא מתאים למשפחה עם ילדים קטנים.");
+		d3.setGender(Gender.MALE);
+		d3.setSize(Size.BIG);
+		dogs.add(d3);
+		
+		Dog d4 = new Dog();
+		d4.setId("4");
+		d4.setName("לאון");
+		d4.setColor("White");
+		d4.setAge((short)9);
+		d4.setArea(Area.CENTER);
+		d4.setBreed(DogBreeds.GoldenRetriever);
+		User user4 = new User("רמוס","סרחיו");		
+//		d4.setOwner(user4);
+	//	d4.setPicture("../../assets/images/4.jpg");
+		d4.setDescription("מדובר בליאון- כלב גולדן מעורב, בן כמעט 9, מסורס ומחוסן. עושה צרכים בחוץ, מאולף לאחר קורס מותאם לאנשים בעלי מוגבלויות. חמוד חמוד חמוד, אוהב חיבוקים ופינוקים!! בגלל רילוקשיין מחפשים לו בית חם ואוהב..");
+		d4.setGender(Gender.MALE);
+		d4.setSize(Size.BIG);
+		dogs.add(d4);
+		
+		Dog d6 = new Dog();
+		d6.setId("6");
+		d6.setName("רודי");
+		d6.setColor("Brown");
+		d6.setAge((short)0.2);
+		d6.setArea(Area.NORTH);
+		d6.setBreed(DogBreeds.GermanShepherd);
+		User user6 = new User("מרסלו","מר");		
+	//	d6.setOwner(user6);
+	//	d6.setPicture("../../assets/images/6.jpg");
+		d6.setDescription("רודי. רודי חמודי התינוק המתוק בן חודשיים. מתלטף ואוהב. מסתדר עם כולם. יהיה בינוני פלוס. נמצא באומנה בחיפה...");
+		d6.setGender(Gender.MALE);
+		d6.setSize(Size.MEDIUM);
+		dogs.add(d6);
+		
+		Dog d8 = new Dog();
+		d8.setId("8");
+		d8.setName("לאקי");
+		d8.setColor("Black");
+		d8.setAge((short)10);
+		d8.setArea(Area.SOUTH);
+		d8.setBreed(DogBreeds.BorderCollie);
+		User user8 = new User("וראן","רפאל");		
+//		d8.setOwner(user8);
+	//	d8.setPicture("../../assets/images/8.jpg");
+		d8.setDescription("לאקי בן 10, כלב מדהים מחונך ונאמן. אנרגטי מאוד. לצערי לי ולמשפחתי אין את היכולת לטפל בו ואנחנו לא רוצים להחזיר אותו לעמותה אם תוכלי לאמץ אותו ולהפוך אותו לחלק מהמשפחה שלך הוא יהיה המאושר בכלבים! הוא כלב מדהים שיודע להעניק חום ואהבה");
+		d8.setGender(Gender.MALE);
+		d8.setSize(Size.BIG);
+		dogs.add(d8);
+			
+		Dog d10 = new Dog();
+		d10.setId("10");
+		d10.setName("מלאני");
+		d10.setColor("Brown");
+		d10.setAge((short)10);
+		d10.setArea(Area.SOUTH);
+		d10.setBreed(DogBreeds.AmericanPitBullTerrier);
+		User user10 = new User("פרננדס","נאצ'ו");		
+//		d10.setOwner(user10);
+//		d10.setPicture("../../assets/images/10.jpg");
+		d10.setDescription("מלאני ,כלבת פיטבול מלאכית ומיוחדת. מלאני חברותית ומסתדרת מצוין עם כלבים ובני אדם, בת כשנה וחצי ובגודל בינוני . מלאני מעוקרת ומחוסנת מלא ותמסר בסל אימוץ. נמצאת באומנה באיזור הצפון . !!!!");
+		d10.setGender(Gender.FEMALE);
+		d10.setSize(Size.BIG);
+		dogs.add(d10);
+		
+		Dog d11 = new Dog();
+		d11.setId("11");
+		d11.setName("דונאט");
+		d11.setColor("Black");
+		d11.setAge((short)0.10);
+		d11.setArea(Area.SOUTH);
+		d11.setBreed(DogBreeds.Greyhound);
+		User user11 = new User("מוראטה","אלבארו");		
+	//	d11.setOwner(user11);
+		//d11.setPicture("../../assets/images/11.jpg");
+		d11.setDescription("דונאט, הכלב הכי הכי מתוק שיש- יותר מסופגנייה. חמודיק כזה בן 10 חודשים. גורון מתבגר מקסים מקסים מקסים. אוהב פינוקים וחיבוקים ונישוקים, אוהב לשחק עם כלבים אחרים, יש לו מבט ממיס עם שתי העיניים הקרובות והמצחיקות שלו והנביחות הכי חמודות שתשמעו. דונאט חייב בית מאמץ  או אומנה מיידית");
+		d11.setGender(Gender.MALE);
+		d11.setSize(Size.BIG);
+		dogs.add(d11);
 		
 		String s = g.toJson(dogs);
 		return s;
 	}
 	
 	@GET 
-    @Path("/getEnumAsJson") 
+    @Path("/getEnum") 
 	@Produces("application/json;charset=utf-8")
-	public String getEnumAsJson(@QueryParam("enumName")String enumName) {
-		String s = new String();
-		
+	public String getEnum(@QueryParam("enumName")String enumName) throws ClassNotFoundException {
+		String s = null;
+
 		try {
 			if ("Animals".equals(enumName))
 				s = new ObjectMapper().writeValueAsString(Animals.values());
@@ -161,6 +271,7 @@ public class GetPetServices {
 			
 		} catch (JsonProcessingException e) {
 			e.printStackTrace();
+			s= e.getMessage();
 		}
 		
 		return s;
